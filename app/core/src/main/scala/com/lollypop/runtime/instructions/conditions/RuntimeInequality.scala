e08d377bf6423393751a94fe085a166e53ce76d7
Lawrence Daniels <lawrence.daniels@gmail.com>
2023-11-07 17:12:51 -0800

    RuntimeInequality: boilerplate cleanup

--- app/core/src/main/scala/com/lollypop/runtime/instructions/conditions/RuntimeInequality.scala
+++ app/core/src/main/scala/com/lollypop/runtime/instructions/conditions/RuntimeInequality.scala
@@ -21,6 +21,48 @@ object RuntimeInequality {
    * @param optionA the host field or value
    */
   final implicit class OptionComparator(val optionA: Option[Any]) extends AnyVal {
+    // TODO convert to Any >= Any
+
+    def >[A <: Comparable[A]](optionB: Option[Any]): Boolean = boilerplate(optionB) {
+      case (a: Date, b: Date) => a.compareTo(b) > 0
+      case (a: Date, b: Number) => a.getTime > b.doubleValue()
+      case (a: Number, b: Date) => a.doubleValue() > b.getTime
+      case (a: Number, b: Number) => a.doubleValue() > b.doubleValue()
+      case (a: String, b: String) => a.compareTo(b) > 0
+      case (a: UUID, b: UUID) => a.compareTo(b) > 0
+      case (a, b) => fail(a, b, operator = ">")
+    }
+
+    def >=[A <: Comparable[A]](optionB: Option[Any]): Boolean = boilerplate(optionB) {
+      case (a: Date, b: Date) => a.compareTo(b) >= 0
+      case (a: Date, b: Number) => a.getTime >= b.doubleValue()
+      case (a: Number, b: Date) => a.doubleValue() >= b.getTime
+      case (a: Number, b: Number) => a.doubleValue() >= b.doubleValue()
+      case (a: String, b: String) => a.compareTo(b) >= 0
+      case (a: UUID, b: UUID) => a.compareTo(b) >= 0
+      case (a, b) => fail(a, b, operator = ">=")
+    }
+
+    def <(optionB: Option[Any]): Boolean = boilerplate(optionB) {
+      case (a: Char, b: Char) => a.compareTo(b) < 0
+      case (a: Date, b: Date) => a.compareTo(b) < 0
+      case (a: Date, b: Number) => a.getTime < b.doubleValue()
+      case (a: Number, b: Date) => a.doubleValue() < b.getTime
+      case (a: Number, b: Number) => a.doubleValue() < b.doubleValue()
+      case (a: String, b: String) => a.compareTo(b) < 0
+      case (a: UUID, b: UUID) => a.compareTo(b) < 0
+      case (a, b) => fail(a, b, operator = "<")
+    }
+
+    def <=[A <: Comparable[A]](optionB: Option[Any]): Boolean = boilerplate(optionB) {
+      case (a: Date, b: Date) => a.compareTo(b) <= 0
+      case (a: Date, b: Number) => a.getTime <= b.doubleValue()
+      case (a: Number, b: Date) => a.doubleValue() <= b.getTime
+      case (a: Number, b: Number) => a.doubleValue() <= b.doubleValue()
+      case (a: String, b: String) => a.compareTo(b) <= 0
+      case (a: UUID, b: UUID) => a.compareTo(b) <= 0
+      case (a, b) => fail(a, b, operator = "<=")
+    }
 
     @tailrec
     def unwrapMe: Option[Any] = optionA match {
@@ -28,73 +70,15 @@ object RuntimeInequality {
       case x => x
     }
 
-    def >[A <: Comparable[A]](optionB: Option[Any]): Boolean = {
+    private def boilerplate(optionB: Option[Any])(f: PartialFunction[(Any, Any), Boolean]): Boolean = {
       (for {
-        valueA <- optionA.unwrapMe
-        valueB <- optionB.unwrapMe
-      } yield {
-        (valueA, valueB) match {
-          case (a: Date, b: Date) => a.compareTo(b) > 0
-          case (a: Date, b: Number) => a.getTime > b.doubleValue()
-          case (a: Number, b: Date) => a.doubleValue() > b.getTime
-          case (a: Number, b: Number) => a.doubleValue() > b.doubleValue()
-          case (a: String, b: String) => a.compareTo(b) > 0
-          case (a: UUID, b: UUID) => a.compareTo(b) > 0
-          case (a, b) => die(s"Could not compare: '$a' > '$b' (${Option(a).map(_.getClass.getName).orNull}, ${Option(b).map(_.getClass.getName).orNull})")
-        }
-      }).contains(true)
+        a <- optionA.unwrapMe
+        b <- optionB.unwrapMe
+      } yield f(a, b)).contains(true)
     }
 
-    def >=[A <: Comparable[A]](optionB: Option[Any]): Boolean = {
-      (for {
-        valueA <- optionA.unwrapMe
-        valueB <- optionB.unwrapMe
-      } yield {
-        (valueA, valueB) match {
-          case (a: Date, b: Date) => a.compareTo(b) >= 0
-          case (a: Date, b: Number) => a.getTime >= b.doubleValue()
-          case (a: Number, b: Date) => a.doubleValue() >= b.getTime
-          case (a: Number, b: Number) => a.doubleValue() >= b.doubleValue()
-          case (a: String, b: String) => a.compareTo(b) >= 0
-          case (a: UUID, b: UUID) => a.compareTo(b) >= 0
-          case (a, b) => die(s"Could not compare: '$a' >= '$b' (${Option(a).map(_.getClass.getName).orNull}, ${Option(b).map(_.getClass.getName).orNull})")
-        }
-      }).contains(true)
-    }
-
-    def <(optionB: Option[Any]): Boolean = {
-      (for {
-        valueA <- optionA.unwrapMe
-        valueB <- optionB.unwrapMe
-      } yield {
-        (valueA, valueB) match {
-          case (a: Char, b: Char) => a.compareTo(b) < 0
-          case (a: Date, b: Date) => a.compareTo(b) < 0
-          case (a: Date, b: Number) => a.getTime < b.doubleValue()
-          case (a: Number, b: Date) => a.doubleValue() < b.getTime
-          case (a: Number, b: Number) => a.doubleValue() < b.doubleValue()
-          case (a: String, b: String) => a.compareTo(b) < 0
-          case (a: UUID, b: UUID) => a.compareTo(b) < 0
-          case (a, b) => die(s"Could not compare: '$a' < '$b' (${Option(a).map(_.getClass.getName).orNull}, ${Option(b).map(_.getClass.getName).orNull})")
-        }
-      }).contains(true)
-    }
-
-    def <=[A <: Comparable[A]](optionB: Option[Any]): Boolean = {
-      (for {
-        valueA <- optionA.unwrapMe
-        valueB <- optionB.unwrapMe
-      } yield {
-        (valueA, valueB) match {
-          case (a: Date, b: Date) => a.compareTo(b) <= 0
-          case (a: Date, b: Number) => a.getTime <= b.doubleValue()
-          case (a: Number, b: Date) => a.doubleValue() <= b.getTime
-          case (a: Number, b: Number) => a.doubleValue() <= b.doubleValue()
-          case (a: String, b: String) => a.compareTo(b) <= 0
-          case (a: UUID, b: UUID) => a.compareTo(b) <= 0
-          case (a, b) => die(s"Could not compare: '$a' <= '$b' (${Option(a).map(_.getClass.getName).orNull}, ${Option(b).map(_.getClass.getName).orNull})")
-        }
-      }).contains(true)
+    private def fail(a: Any, b: Any, operator: String): Nothing = {
+      die(s"Could not compare: '$a' $operator '$b' (${Option(a).map(_.getClass.getName).orNull}, ${Option(b).map(_.getClass.getName).orNull})")
     }
 
   }
