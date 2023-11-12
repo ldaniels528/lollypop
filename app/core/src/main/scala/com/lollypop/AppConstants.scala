cf5f3ed610640dd5f41e8bea2f70a565b8b7f874
Lawrence Daniels <lawrence.daniels@gmail.com>
2023-11-12 00:18:47 -0800

    bumped version up to v0.1.6.4

--- app/core/src/main/scala/com/lollypop/AppConstants.scala
+++ app/core/src/main/scala/com/lollypop/AppConstants.scala
@@ -10,7 +10,7 @@ trait AppConstants {
   val MAJOR_VERSION: Int = 0
   val MINOR_VERSION: Int = 1
   val MINI_VERSION: Int = 6
-  val MICRO_VERSION: Int = 3
+  val MICRO_VERSION: Int = 4
 
   val version = s"$MAJOR_VERSION.$MINOR_VERSION.$MINI_VERSION.$MICRO_VERSION"
 
