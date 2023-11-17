c102b9171b7f4c348b41666df08e56d8eaede80b
Lawrence Daniels <lawrence.daniels@gmail.com>
2023-11-17 13:49:17 -0800

    added implicit class ProductToTableType

--- app/core/src/main/scala/com/lollypop/runtime/instructions/queryables/ProductTableRendering.scala
+++ app/core/src/main/scala/com/lollypop/runtime/instructions/queryables/ProductTableRendering.scala
@@ -5,14 +5,30 @@ import com.lollypop.runtime.datatypes.Inferences.fromClass
 import com.lollypop.runtime.datatypes.TableType
 import com.lollypop.runtime.devices.RowCollectionZoo.ProductToRowCollection
 import com.lollypop.runtime.devices.{RowCollection, TableColumn}
+import com.lollypop.runtime.instructions.queryables.ProductTableRendering.implicits.ProductToTableType
 import com.lollypop.runtime.plastics.RuntimeClass.implicits.RuntimeClassProduct
 
 trait ProductTableRendering extends TableRendering { self: Product =>
 
   override def toTable(implicit scope: Scope): RowCollection = self.toRowCollection
 
-  def toTableType: TableType = {
-    TableType(columns = self.productElementFields.map { field => TableColumn(field.getName, `type` = fromClass(field.getType)) })
+  def toTableType: TableType = self.asTableType
+
+}
+
+object ProductTableRendering {
+
+  object implicits {
+
+    final implicit class ProductToTableType[A <: Product](val self: A) extends AnyVal {
+      @inline
+      def asTableType: TableType = {
+        TableType(columns = self.productElementFields.map { field =>
+          TableColumn(field.getName, `type` = fromClass(field.getType))
+        })
+      }
+    }
+
   }
 
 }
