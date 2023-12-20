05143c1919a27c7e337f4b522aa6eb3ce236c16f
Lawrence Daniels <lawrence.daniels@gmail.com>
2023-12-20 14:58:14 -0800

    Column name fix in toSQL

--- app/core/src/main/scala/com/lollypop/language/models/OrderColumn.scala
+++ app/core/src/main/scala/com/lollypop/language/models/OrderColumn.scala
@@ -1,10 +1,12 @@
 package com.lollypop.language.models
 
+import com.lollypop.language.LifestyleExpressions
+
 /**
  * Represents an Ordered Column
  * @param name        the name of the column
  * @param isAscending indicates whether the column is ascending (or conversely descending)
  */
 case class OrderColumn(name: String, isAscending: Boolean) extends Instruction {
-  override def toSQL: String = Seq(name, if (isAscending) "asc" else "desc").mkString(" ")
+  override def toSQL: String = Seq(name.a.toSQL, if (isAscending) "asc" else "desc").mkString(" ")
 }
